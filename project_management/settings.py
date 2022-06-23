57efdd40f8b970b17f1a0c12b07e8cb8ba2ae716
AliJabrailzade <jabrailzadeali@gmail.com>
2022-06-23 22:07:02 +0400

    StaticFiles_STORAGE added

--- project_management/settings.py
+++ project_management/settings.py
@@ -137,6 +137,8 @@ STATICFILES_DIRS = [
 	BASE_DIR / 'static'
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
