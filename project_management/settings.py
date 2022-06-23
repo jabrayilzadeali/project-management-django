90188b82d1bce40fccf7e4b457b457bfc014590d
AliJabrailzade <jabrailzadeali@gmail.com>
2022-06-23 21:36:18 +0400

    Setup for heroku deploy
    - added some additional packages to requirements.txt
    - Added runtime.txt
    - added procfile
    
    In project_management/settings.py
    - ALLOWED_HOSTS = ['project-management-django4.herokuapp.com', '127.0.0.1']
    - debug = False

--- project_management/settings.py
+++ project_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q0^1%yolt*)94!*=pzotj$459acl&$pes@dj_15tw(!ghr3@#%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-management-django4.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
